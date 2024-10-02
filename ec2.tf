9a9662a98ba72d31bbb70abb8b9eb91255059c7e
Neelima Avala <neelimaavala2002@gmail.com>
2024-10-02 16:25:59 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     aws = {
@@ -9,42 +10,55 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
-}  
+}
 resource "aws_security_group" "neelimasg" {
-    name = "neelimasg"
-    description = "create sg for instance"
-    ingress  {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+  name        = "neelimasg"
+  description = "create sg for instance"
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
     }
-    
-    ingress  {
-        from_port = 22
-        to_port = 22
-        protocol ="tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+ 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol   = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     }
-    ingress  {
-        from_port = 80
-        to_port = 80
-        protocol ="tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol   = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_vpc" "neelimavpc" {
-    cidr_block = ["10.0.0.0/16"]
+    cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = true
     tags = {
       Name = "neelima_vpc"
     }
 }
+resource "aws_subnet" "neelimasubnet" {
+    vpc_id = aws_vpc.neelimavpc.id
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "neelimasubnet"
+    }
+  
+}
 resource "aws_instance" "neelima" {
   ami           = "ami-0dee22c13ea7a9a67"
   instance_type = "t2.micro"
   tags = {
-  name = "Myinstance" }
+  Name = "Myinstance" }
   security_groups = [aws_security_group.neelimasg.name]
 }
